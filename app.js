97cc22c934ee30c121c917771d0e6bf41d561aa6
Magdalena <malehernandez44@gmail.com>
2025-03-17 15:05:36 -0600

    mostrar la lista de amigos

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let Lista_amigos = [];
 
-//Lista de nombres
-let Lista_nombres= [];
-alert
+function agregarAmigo() {
+// Obtener el elemento input por su ID
+    let Nombre_ingresado = document.getElementById("amigo");
+// Obtener lo que el usuario escribio en el input
+    let nombreDelAmigo = Nombre_ingresado.value;
+// Agregar los nombres a la lista
+    Lista_amigos.push(nombreDelAmigo);
+    console.log("Lista de amigos actual:", Lista_amigos);
+    mostrarListaDeAmigos();
+}
+// Mostar la lista de amigos
+function mostrarListaDeAmigos() {
+    let mostrar_lista = document.getElementById("listaAmigos"); //  "listaAmigos" en HTML
+    if (mostrar_lista) {
+      mostrar_lista.innerHTML = "";
+      if (Lista_amigos.length > 0) {
+        let listaUl = document.createElement("ul");
+        Lista_amigos.forEach(amigo => {
+          let nuevoLi = document.createElement("li");
+          nuevoLi.textContent = amigo;
+          listaUl.appendChild(nuevoLi);
+        });
+        mostrar_lista.appendChild(listaUl);
+      } else {
+        mostrar_lista.textContent = "Aún no has agregado ningún amigo.";
+      }
+    }
+  }
 
-console.log(Lista_nombres)
+document.addEventListener('DOMContentLoaded', mostrarListaDeAmigos);
 
+
+   
+  
+    
 
 
 
